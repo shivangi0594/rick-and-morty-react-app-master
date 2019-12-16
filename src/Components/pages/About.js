2e9891c5003674d950abea079cc24f46afa1183b
shivangi0594 <57615044+shivangi0594@users.noreply.github.com>
2019-12-17 00:18:18 +0530

    Create About.js

---
+++ src/Components/pages/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function About() {
+    return (
+        <section className="page-section">
+            <div className="container">
+                <h2 className="section-title">About page</h2>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Laborum quisquam impedit alias, optio suscipit sequi
+                    officia, et expedita repudiandae cumque temporibus
+                    voluptates. Porro aut facilis commodi, minus laborum earum
+                    modi!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Perspiciatis voluptatibus pariatur ab, incidunt vero
+                    reprehenderit eaque corrupti quisquam aspernatur velit
+                    quibusdam expedita quasi nisi? Perspiciatis totam eaque
+                    saepe placeat error.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
